e7d96171abde2413e00a026f1d16068df59f3fc0
MagicPojska <pojskicsafet@gmail.com>
2021-08-05 12:18:37 +0200

    Added javascript reveal scripts

--- assets/js/app.js
+++ assets/js/app.js
@@ -54,3 +54,35 @@ function writeText() {
 }
 
 setInterval(writeText, 150);
+
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home_title', {});
+sr.reveal('.button', { delay: 200 });
+sr.reveal('.home_img', { delay: 400 });
+sr.reveal('.home_social-icon', { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal('.about_img', {});
+sr.reveal('.about_subtitle', { delay: 400 });
+sr.reveal('.about_text', { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills_subtitle', {});
+sr.reveal('.skills_text', {});
+sr.reveal('.skills_data', { interval: 200 });
+sr.reveal('.skills_img', { delay: 600 });
+
+/*SCROLL WORK*/
+sr.reveal('.work_img', { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact_input', { interval: 200 });
