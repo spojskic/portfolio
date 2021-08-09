6f689a1dca66054afd6eb9da63c9064d740955f4
Sam Sepiel <pojskicsafet@gmail.com>
2021-08-09 21:07:39 +0200

    Testing load instead of domcontentloaded

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,127 +1,123 @@
 /*Intro animation*/
-const intro = document.querySelector('.intro');
-const logo = document.querySelector('.logo-header');
-const logoSpan = document.querySelectorAll('.logo');
+const intro = document.querySelector(".intro");
+const logo = document.querySelector(".logo-header");
+const logoSpan = document.querySelectorAll(".logo");
 
+window.addEventListener("load", () => {
+  logoSpan.forEach((span, idx) => {
+    setTimeout(() => {
+      span.classList.add("active");
+    }, (idx + 1) * 90);
+  });
 
-window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
     logoSpan.forEach((span, idx) => {
-        setTimeout(() => {
-            span.classList.add('active');
-        }, (idx + 1) * 90)
-    })
+      setTimeout(() => {
+        span.classList.remove("active");
+        span.classList.add("fade");
+      }, (idx + 1) * 80);
+    });
+  }, 1500);
 
-    setTimeout(() => {
-        logoSpan.forEach((span, idx) => {
-            setTimeout(() => {
-                span.classList.remove('active');
-                span.classList.add('fade');
-            }, (idx + 1) * 80)
-        })
-    }, 1500);
-
-    setTimeout(() => {
-        intro.style.top = '-100vh';
-    }, 2000);
-})
-
+  setTimeout(() => {
+    intro.style.top = "-100vh";
+  }, 2000);
+});
 
 /*Show menu*/
 const showMenu = (toggleId, navID) => {
-    const toggle = document.getElementById(toggleId);
-    nav = document.getElementById(navID);
+  const toggle = document.getElementById(toggleId);
+  nav = document.getElementById(navID);
 
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show_menu')
-        })
-    }
-}
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show_menu");
+    });
+  }
+};
 
-showMenu('nav-toggle', 'nav-menu');
-
+showMenu("nav-toggle", "nav-menu");
 
 /*Active and remove menu*/
-const navLink = document.querySelectorAll('.nav_link');
+const navLink = document.querySelectorAll(".nav_link");
 
 function linkAction() {
-    navLink.forEach(n => n.classList.remove('active'));
-    this.classList.add('active');
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
 
-    /*This is for removing menu on click on mobile*/
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show_menu');
+  /*This is for removing menu on click on mobile*/
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show_menu");
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*Auto write text*/
-const iAm = document.getElementById('autoWrite');
+const iAm = document.getElementById("autoWrite");
 let index = 0;
 
-const texts = ['Web Developer    ', 'Gym Addict    ', 'Anime Fan    ', 'Gamer    ']
+const texts = [
+  "Web Developer    ",
+  "Gym Addict    ",
+  "Anime Fan    ",
+  "Gamer    ",
+];
 
 let wordCounter = 0;
 let text = texts[wordCounter];
 
 function writeText() {
-    iAm.innerText = text.slice(0, index);
-    index++;
+  iAm.innerText = text.slice(0, index);
+  index++;
 
-    if (index > text.length) {
-        index = 1;
-        wordCounter++;
-        text = texts[wordCounter];
-        if (wordCounter === 3) {
-            wordCounter = 0;
-            text = texts[wordCounter];
-        }
+  if (index > text.length) {
+    index = 1;
+    wordCounter++;
+    text = texts[wordCounter];
+    if (wordCounter === 3) {
+      wordCounter = 0;
+      text = texts[wordCounter];
     }
+  }
 }
 
 setInterval(writeText, 150);
 
-
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '40px',
-    duration: 2000,
-    reset: true
+  origin: "top",
+  distance: "40px",
+  duration: 2000,
+  reset: true,
 });
 
-
 /*SCROLL HOME*/
-sr.reveal('.home_title', {});
-sr.reveal('.button', { delay: 200 });
-sr.reveal('.home_img', { delay: 400 });
-sr.reveal('.home_social-icon', { interval: 200 });
+sr.reveal(".home_title", {});
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home_img", { delay: 400 });
+sr.reveal(".home_social-icon", { interval: 200 });
 
 /*SCROLL ABOUT*/
-sr.reveal('.about_img', {});
-sr.reveal('.about_subtitle', { delay: 400 });
-sr.reveal('.about_text', { delay: 400 });
+sr.reveal(".about_img", {});
+sr.reveal(".about_subtitle", { delay: 400 });
+sr.reveal(".about_text", { delay: 400 });
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills_subtitle', {});
-sr.reveal('.skills_text', {});
-sr.reveal('.skills_data', { interval: 200 });
-sr.reveal('.skills_img', { delay: 600 });
+sr.reveal(".skills_subtitle", {});
+sr.reveal(".skills_text", {});
+sr.reveal(".skills_data", { interval: 200 });
+sr.reveal(".skills_img", { delay: 600 });
 
 /*SCROLL WORK*/
-sr.reveal('.work_img', { interval: 200 });
+sr.reveal(".work_img", { interval: 200 });
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact_input', { interval: 200 });
-
+sr.reveal(".contact_input", { interval: 200 });
 
 /*Toggle dark mode*/
-const toggle = document.getElementById('toggle');
+const toggle = document.getElementById("toggle");
 
-
-toggle.addEventListener('change', (e) => {
-    document.body.classList.toggle('dark', e.target.checked);
-    document.getElementById('head').classList.toggle('dark');
-})
-
-
+toggle.addEventListener("change", (e) => {
+  document.body.classList.toggle("dark", e.target.checked);
+  document.getElementById("head").classList.toggle("dark");
+});
