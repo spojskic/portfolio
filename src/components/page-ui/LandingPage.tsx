80cdc9bd638d9a61020e6e6017d25183fde474b5
spojskic <pojskicsafet@gmail.com>
2024-03-03 13:22:09 +0100

    Moved component outside of landing page component

--- src/components/page-ui/LandingPage.tsx
+++ src/components/page-ui/LandingPage.tsx
@@ -24,22 +24,27 @@ const words = [
 
 export function LandingPage() {
     return (
-        <div
-            id="landing-page"
-            className="bg- relative flex  h-screen w-full flex-col items-center justify-center bg-white antialiased bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]"
-        >
-            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)] dark:bg-black"></div>
-            <p className="text-xs text-neutral-600 dark:text-neutral-200 sm:text-base  "> Hello, I&apos;m Safet. A passionate Software Engineer.</p>
-            <TypewriterEffectSmooth words={words} />
-            <div className="z-30 flex flex-col items-center space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-                <Link href="https://drive.google.com/file/d/1xHPigW5eJQSCCgx3VBFyH5E0y_wwOdDN/view?usp=sharing">
-                    <Button>Download CV</Button>
-                </Link>
-                <Link href="#contact-me">
-                    <Button variant="secondary">Contact Me</Button>
-                </Link>
+        <>
+            <div
+                id="landing-page"
+                className="bg- relative flex  h-screen w-full flex-col items-center justify-center bg-white bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]"
+            >
+                <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)] dark:bg-black"></div>
+                <p className="text-xs text-neutral-600 dark:text-neutral-200 sm:text-base  ">
+                    {' '}
+                    Hello, I&apos;m Safet. A passionate Software Engineer.
+                </p>
+                <TypewriterEffectSmooth words={words} />
+                <div className="z-30 flex flex-col items-center space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+                    <Link href="https://drive.google.com/file/d/1xHPigW5eJQSCCgx3VBFyH5E0y_wwOdDN/view?usp=sharing">
+                        <Button>Download CV</Button>
+                    </Link>
+                    <Link href="#contact-me">
+                        <Button variant="secondary">Contact Me</Button>
+                    </Link>
+                </div>
             </div>
             <BackgroundBeams />
-        </div>
+        </>
     );
 }
